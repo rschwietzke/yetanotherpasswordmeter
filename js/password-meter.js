ca729fbc307ec770d987ae8030ffe83c6c5ad80e
Rene Schwietzke <mail@03146f06.net>
2014-09-02 22:22:53 +0200

    Adjusted license in code header

--- js/password-meter.js
+++ js/password-meter.js
@@ -5,25 +5,29 @@
 **    Last modified: 2014-08-20
 **    Version: 2.0.0
 **
-**    License Information:
+**    The MIT License (MIT)
 **    -------------------------------------------------------------------------
 **    Copyright (C) 2014 Rene Schwietzke
 **
-**    This program is free software; you can redistribute it and/or modify it
-**    under the terms of the GNU General Public License as published by the
-**    Free Software Foundation; either version 2 of the License, or (at your
-**    option) any later version.
+**    Permission is hereby granted, free of charge, to any person obtaining a copy
+**    of this software and associated documentation files (the "Software"), to deal
+**    in the Software without restriction, including without limitation the rights
+**    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+**    copies of the Software, and to permit persons to whom the Software is
+**    furnished to do so, subject to the following conditions:
+**    
+**    The above copyright notice and this permission notice shall be included in
+**    all copies or substantial portions of the Software.
+
+**    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+**    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+**    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+**    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+**    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+**    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+**    THE SOFTWARE.
 **
-**    This program is distributed in the hope that it will be useful, but
-**    WITHOUT ANY WARRANTY; without even the implied warranty of
-**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-**    General Public License for more details.
-**
-**    You should have received a copy of the GNU General Public License along
-**    with this program; if not, write to the Free Software Foundation, Inc.,
-**    59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-**
-**    Based on original work by Jeff Todnem published under GPL 2.
+**    Inspired by work by Jeff Todnem published under GPL 2.
 **
 **    Original File: pwd_meter.js (http://www.passwordmeter.com/)
 **    Created by: Jeff Todnem (http://www.todnem.com/)
@@ -41,14 +45,13 @@
 **    v2.0.0 : Pay more attention to length.
 **             Tests
 **             Refactored code
+**             Changed license to MIT-license
 **
 **    ToDo:
 **    -------------------------------------------------------------------------
 **    * Punish first or last letter uppercase characters only
-**    * Punish special characters only at the end
 **    * Punish numbers only at the end
 **    * Filter common patterns, such as 12.12.2008 12/20/2009 2008-12-13
-**    * Seem to contain a year 19XX or 20XX
 **/
 PasswordMeter.prototype = (
 {
